e78b37776d73e24b3256195855af2349931b2c04
karan <karanchy95@gmail.com>
2023-05-08 13:00:48 +0545

    news section added

--- src/components/Nav1.tsx
+++ src/components/Nav1.tsx
@@ -7,10 +7,9 @@ function Nav1() {
   const handleDropDown = () => {
     SetAbout(!about);
   };
-  const handleDropDownHide =()=>{
-    SetAbout(false)
-
-  }
+  const handleDropDownHide = () => {
+    SetAbout(false);
+  };
   return (
     <>
       <div className="container mx-auto lg:hidden">
@@ -43,56 +42,82 @@ function Nav1() {
         <div className="">
           <ul className="flex flex-row items-center font-[inter] gap-[40px] font-normal text-[15px] uppercase leading-[14.52px] text-[#000000]">
             <li>
-              <Link href="/" className="font-semibold  hover:text-[#1CABD3] transition-colors duration-150 ease-out">
+              <Link
+                href="/"
+                className="font-semibold  hover:text-[#1CABD3] transition-colors duration-150 ease-out"
+              >
                 Home
               </Link>
             </li>
-            <li onMouseEnter={handleDropDown} onMouseLeave={handleDropDownHide} className="relative">
-              <Link href="/about" className=" font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out">
-                About Us
+            <li
+              onMouseEnter={handleDropDown}
+              onMouseLeave={handleDropDownHide}
+              className="relative"
+            >
+              <Link
+                href="/about"
+                className=" font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out"
+              >
+                Company
               </Link>
 
               {about && (
-                <div className="absolute top-[14px] left-0  w-[150px] h-[56px] z-40 space-y-[10px]">
-                  <div className="bg-white border   absolute top-[3px] h-[100%] w-full pt-[10px] pl-[15px]">
+                <div className="absolute top-[18px] left-0  w-[199px] h-[126px] z-40 space-y-[10px]">
+                  <div className="bg-white border   absolute top-[8px] h-[100%] w-full pt-[15px] pl-[15px]">
                     <li className="text-[15px] hover:text-[#1CABD3] transition-colors duration-300 ease-out font-semibold cursor-pointer">
-                      Company
+                      <Link href="/about">About Web Trading</Link>
                     </li>
-                    <li className="text-[15px] mt-[6px] hover:text-[#1CABD3] transition-colors duration-300 ease-out font-semibold whitespace-pre cursor-pointer">
-                      <Link href="/team">
-                      Our Team
-                      </Link>
+                    <li className="text-[15px] mt-[8px] hover:text-[#1CABD3] transition-colors duration-300 ease-out font-semibold whitespace-pre cursor-pointer">
+                      <Link href="">Meet our CEO</Link>
+                    </li>
+                    <li className="text-[15px] mt-[8px] hover:text-[#1CABD3] transition-colors duration-300 ease-out font-semibold whitespace-pre cursor-pointer">
+                      <Link href="/team">Our Team</Link>
+                    </li>
+                    <li className="text-[15px] mt-[8px] hover:text-[#1CABD3] transition-colors duration-300 ease-out font-semibold whitespace-pre cursor-pointer">
+                      <Link href="">Our Network</Link>
                     </li>
                   </div>
                 </div>
               )}
             </li>
             <li>
-              <Link href="" className=" font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out">
+              <Link
+                href="/news"
+                className=" font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out"
+              >
                 News & Event
               </Link>
             </li>
             <li>
-              <Link href="/ielts" className="font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out">
+              <Link
+                href="/ielts"
+                className="font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out"
+              >
                 Product Category
               </Link>
             </li>
             <li>
-              <Link href="" className="font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out">
+              <Link
+                href=""
+                className="font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out"
+              >
                 Career
               </Link>
             </li>
             <li>
-              <Link href="" className=" font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out">
-                Check Mail
+              <Link
+                href="/contact"
+                className=" font-semibold hover:text-[#1CABD3] transition-colors duration-300 ease-out"
+              >
+                Contact Us
               </Link>
             </li>
             <li className="h-[42px] w-[120px] bg-[#1CABD3] rounded-[39px] flex items-center justify-center">
               <Link
-                href="/contact"
+                href=""
                 className=" text-white text-[15px] leading-[18.15px] font-semibold"
               >
-                Contact Us
+                Check Mail
               </Link>
             </li>
           </ul>
