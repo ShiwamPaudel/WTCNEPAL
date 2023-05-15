c67319c0908332adc9e0a9942a22a9630137b736
karan <karanchy95@gmail.com>
2023-05-15 17:08:24 +0545

    changes

--- src/components/AboutBanner.tsx
+++ src/components/AboutBanner.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutBanner = ({title}:any) => {
   return (
     <div className='relative'>
-    <img src="/../assets/about.png" className='w-full h-[150px] md:h-auto' alt="" />
+    <img src="/../assets/aboutbanner.jpg" className='w-full h-[150px] md:h-[280px] object-cover' alt="" />
     <h1 className='text-[#FEFEFE] cursor-pointer md:text-[48px] text-[25px] font-bold leading-[60.24px] absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]'>{title}</h1>
 
   </div>
