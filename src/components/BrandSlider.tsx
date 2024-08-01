80a231d7afc74c2a53f7a1b35e3031a23d6b0b27
ShiwamPaudel <106426216+ShiwamPaudel@users.noreply.github.com>
2024-08-01 12:06:08 +0545

    updates: brand logo updates and gap updates

--- src/components/BrandSlider.tsx
+++ src/components/BrandSlider.tsx
@@ -1,22 +1,19 @@
-// BrandSlider.tsx
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-// Define type for brand data
 interface Brand {
   name: string;
   image: string;
   url: string;
 }
 
-// Define slider settings
 const sliderSettings = {
   dots: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 6, // Show 6 slides at a time
+  slidesToShow: 6,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
@@ -24,15 +21,15 @@ const sliderSettings = {
 
 const brands: Brand[] = [
   { name: 'Brand 1', image: '/assets/Partners/DiaSorin.png', url: '' },
-  { name: 'Brand 2', image: '/assets/Partners/asclepion.png', url: '' },
+  { name: 'Brand 2', image: '/assets/Partners/Asclepion.png', url: '' },
   { name: 'Brand 3', image: '/assets/Partners/eppedorf.png', url: '' },
   { name: 'Brand 4', image: '/assets/Partners/diversey.png', url: '' },
   { name: 'Brand 5', image: '/assets/Partners/isens.jpg', url: '' },
   { name: 'Brand 6', image: '/assets/Partners/avantor.png', url: '' },
   { name: 'Brand 7', image: '/assets/Partners/BioSystems.png', url: '' },
-  { name: 'Brand 8', image: '/assets/Partners/newster.png', url: '' },
-  { name: 'Brand 9', image: '/assets/Partners/nihonkohden.png', url: '' },
-  { name: 'Brand 11', image: '/assets/Partners/Ecotex.png', url: '' },
+  { name: 'Brand 8', image: '/assets/Partners/Newster.png', url: '' },
+  { name: 'Brand 9', image: '/assets/Partners/Nihon Kohden.png', url: '' },
+  { name: 'Brand 11', image: '/assets/Partners/ECOTEX.png', url: '' },
 ];
 
 const BrandSlider: React.FC = () => {
