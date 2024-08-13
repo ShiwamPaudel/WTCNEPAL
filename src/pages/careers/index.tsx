5fabc3761b4de333352be04826b970eeb4b463ea
ShiwamPaudel <106426216+ShiwamPaudel@users.noreply.github.com>
2024-08-13 20:08:16 +0545

    updates: terms & conditions and privacy policy updates

--- src/pages/careers/index.tsx
+++ src/pages/careers/index.tsx
@@ -10,13 +10,13 @@ const index = () => {
     <link rel="icon" href="/../assets/favicon.png" />
     <link
       rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+      // href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+      // integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
     />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
+    {/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/> */}
   </Head>
     <div className="container mx-auto 2xl:max-w-[1180px] xl:px-20 2xl:px-0  ">
-      {/* <AboutBanner/> */}
+      <AboutBanner/>
 
       <JobListing />
     </div>
