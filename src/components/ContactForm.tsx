6c65afc42bd62a01a6caaffbb2f29870c4bb3572
Shiwam Paudel <paudel.shiwam@gmail.com>
2026-01-01 14:47:38 +0545

    status chagne js to mjs for baseurl from utils

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useFormik } from "formik";
 import axios from "axios";
 // import { BaseUrl } from "@/pages/api/global";
-import { BaseUrl } from "@/utils/global";
+import { BaseUrl } from "@/utils/global.mjs";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
