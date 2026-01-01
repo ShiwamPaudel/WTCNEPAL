798df9a76f54dc23476977ee8722fa3539aaafee
Shiwam Paudel <paudel.shiwam@gmail.com>
2026-01-01 15:15:25 +0545

    tailwind css issue fixed with contents

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      fontFamily:{
-        'ibm': ['IBM Plex Serif', 'serif'],
-        'inter': ['Inter', 'sans-serif']
-      }
+      fontFamily: {
+        // This sets Poppins as the primary sans-serif font
+        sans: ['Poppins', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
